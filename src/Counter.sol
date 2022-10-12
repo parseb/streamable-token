376f6bffeb154ebc315d714458a4e2745a2ba119
parseb <petra306@protonmail.com>
2022-10-12 20:40:39 +0300

    forge init

---
+++ src/Counter.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract ERC3525streamable {
+    public address owner;
+
+
+    constructor() {
+
+    }
+}
---
+++ test/Counter.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+ 
+
+contract ERC3525STest is Test {
+
+
+    function setUp() public {
+
+    }
+
+}
