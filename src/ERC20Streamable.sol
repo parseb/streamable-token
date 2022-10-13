34c0d5053aaaf0ffbfda55f0c19dd692e362378a
parseb <petra306@protonmail.com>
2022-10-14 01:36:21 +0300

    base

---
+++ src/ERC20Streamable.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "solmate/tokens/ERC20.sol";
+
+struct Stream {
+    address from;
+    uint256[2] timeStartEnd;
+    uint256 perSec;
+}
+
+contract ERC20Streamable is ERC20 {
+    uint256 time0;
+    address public c_c;
+    mapping(bytes4 => bool) isSuspended;
+
+    mapping(address => Stream[]) userStreams;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, uint256 totalMint_)
+        ERC20(name_, symbol_, decimals_)
+    {
+        time0 = block.timestamp;
+        c_c = msg.sender;
+        _mint(msg.sender, totalMint_);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error SuspendedFunction();
+
+    /*//////////////////////////////////////////////////////////////
+                               Events
+    //////////////////////////////////////////////////////////////*/
+    event functionSuspended(bytes4 fxSelect, bool afterFlipState);
+
+    /*//////////////////////////////////////////////////////////////
+                               Public Stream
+    //////////////////////////////////////////////////////////////*/
+
+    function startStream(address to_, uint256 amtPerSec_, uint256 units_) public returns (uint256 startBalance) {
+        Stream memory S;
+        S.from = msg.sender;
+        S.perSec = amtPerSec_;
+        S.timeStartEnd[0] = block.timestamp;
+        if (units_ > 0) S.timeStartEnd[1] = block.timestamp + units_;
+
+        userStreams[msg.sender].push(S);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               Public Override
+    //////////////////////////////////////////////////////////////*/
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        storedBalanceOf[msg.sender] -= amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            storedBalanceOf[to] += amount;
+        }
+
+        emit Transfer(msg.sender, to, amount);
+
+        return true;
+    }
+
+    function balanceOf(address who_) public returns (uint256) {
+        return 5;
+        /// try magic
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
+        uint256 allowed = allowance[from][msg.sender]; // Saves gas for limited approvals.
+
+        if (allowed != type(uint256).max) allowance[from][msg.sender] = allowed - amount;
+
+        storedBalanceOf[from] -= amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            storedBalanceOf[to] += amount;
+        }
+
+        emit Transfer(from, to, amount);
+
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        allowance[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               Internal Override
+    //////////////////////////////////////////////////////////////*/
+
+    function _mint(address to, uint256 amount) internal virtual override {
+        totalSupply += amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            storedBalanceOf[to] += amount;
+        }
+
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal virtual override {
+        storedBalanceOf[from] -= amount;
+
+        // Cannot underflow because a user's balance
+        // will never be larger than the total supply.
+        unchecked {
+            totalSupply -= amount;
+        }
+
+        emit Transfer(from, address(0), amount);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                               View
+    //////////////////////////////////////////////////////////////*/
+
+    /*//////////////////////////////////////////////////////////////
+                               Misc
+    //////////////////////////////////////////////////////////////*/
+
+    function flipSuspended(bytes4 funcSig_) public {
+        require(msg.sender == c_c, "not command and control");
+        isSuspended[funcSig_] = !isSuspended[funcSig_];
+        emit functionSuspended(funcSig_, isSuspended[funcSig_]);
+    }
+}
--- test/Base.t.sol
+++ test/Base.t.sol
@@ -2,22 +2,31 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/ERC3525streamable.sol";
+import "../src/ERC20Streamable.sol";
 
-contract ERC3525STest is Test {
-    ERC3525streamable E3252S;
+contract ERC20StreamableInitTest is Test {
+    ERC20Streamable ERC20S;
     address deployer;
     address notDeployer;
+
+    string name = "StreamableToken";
+    string symbol = "StrT";
+    uint256 totalMint;
 
     function setUp() public {
         deployer = address(16);
         notDeployer = address(160001);
+        totalMint = 1_000_000 * 1 ether;
 
         vm.prank(deployer);
-        E3252S = new ERC3525streamable();
+        ERC20S = new ERC20Streamable(name,symbol,18, totalMint);
     }
 
-    // function testSetsOwner() public {
-    //     assertTrue(E3252S.owner() == deployer, "default owner not deployer");
-    // }
+    function testDeployed() public {
+        assertTrue(
+            keccak256((abi.encodePacked(ERC20S.name()))) == keccak256(abi.encodePacked(name)), "not expected name"
+        );
+
+        assertTrue(ERC20S.totalSupply() == totalMint, "inconsistent total balance");
+    }
 }
